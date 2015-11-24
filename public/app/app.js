53cf313104617021148dc03a9b317fa7b5533f11
francisco-filho <francisco@capimgrosso.com>
2015-11-24 21:54:01 -0200

    basic form with validation

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,37 @@
 (function(){
     'use strict';
 
-    angular.module('myApp', []);
+    angular.module('myApp', [])
+    .controller('signupController', signupController)
+    .directive('ensureUnique', function($timeout){
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, el, attrs, ctrl){
+                if (!ctrl) return;
+                scope.$watch(attrs.ngModel, function(value){
+                    $timeout(function(){
+                        if (value == 'chico'){
+                            ctrl.$setValidity('unique',true);
+                        } else {
+                            ctrl.$setValidity('unique', false);
+                        }
+                    },0);
+                });
+            }
+        }
+    });
+
+    function signupController($scope){
+        $scope.submitted = false;
+
+        $scope.signupForm = function(){
+            console.log('submitting...')
+            if ($scope.signup_form.$valid){
+                console.info('Form valid')
+            } else {
+                $scope.submitted = true;
+            }
+        }
+    }
 })();
